0edec8d8434a40f99c07c8f727f7627aa2dbd893
SuradechSD <122203226+SuradechSD@users.noreply.github.com>
2024-08-08 03:01:59 +0700

    Add files via upload

---
+++ src/Lab1/Form2.java
@@ -0,0 +1,39 @@
+package Lab1;
+
+import java.util.Scanner;
+
+public class Form2 {
+    public static void main(String[] args) {
+
+        int x,y;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter X : ");
+        x = sc.nextInt();
+        System.out.print("Enter Y : ");
+        y =sc.nextInt();
+
+        if (x == 0 && y == 0)
+            System.out.print("Origin");
+
+        else if (x > 0 && y > 0)
+            System.out.print("Q1");
+
+        else if (x < 0 && y > 0)
+            System.out.print("Q2");
+
+        else if (x < 0 && y < 0)
+            System.out.print("Q3");
+
+        else if (x > 0 && y < 0)
+            System.out.print("Q4");
+
+        else if (x == 0 && y > 0)
+            System.out.print("Y-intercept");
+        else if (x < 0 && y == 0)
+            System.out.print("X-intercept");
+        sc.close();  
+    }
+    
+  
+}
---
+++ src/Lab4/Product.java
@@ -0,0 +1,34 @@
+/*package Lab4;
+
+public class Product {
+
+    private String name;
+    private String code;
+    private int price;
+//-------------------------------------------------
+    public String getname(){
+        return name;
+    }
+    public void setname(String name){
+        this.name = name;
+    }
+    public String getcode(){
+        return code;
+    }
+    public void setcode(String code){
+        this.code = code;
+    }
+    public String getprice(){
+        return code;
+    }
+    public void setprice(int price){
+        this.price = price;
+    }
+//-------------------------------------------------
+public Product(String name,String code,int price){
+    this.name = name;
+    this.code = code;
+    this.price = price;    
+    }
+
+}*/
