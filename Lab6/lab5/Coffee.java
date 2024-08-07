68f393a217b7861a32fdb5a0cb070a250b24146f
SuradechSD <122203226+SuradechSD@users.noreply.github.com>
2024-08-07 15:25:06 +0700

    Add files via upload

---
+++ Lab6/lab5/Coffee.java
@@ -0,0 +1,7 @@
+package lab5;
+
+public class Coffee extends Product{
+	public Coffee(int p){
+	super(p);
+	}
+}
---
+++ Lab6/lab5/Sugar.java
@@ -0,0 +1,7 @@
+package lab5;
+
+public class Sugar extends Product {
+	public Sugar(int p){
+	super(p);
+	}
+}
